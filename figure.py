43caa0197f9ec1c34f51237be8bb3b396f7a7465
KmYongHwan <kyh5678@gmail.com>
2023-06-01 03:36:27 +0900

    implement hw5

--- figure.py
+++ figure.py
@@ -1,36 +1,41 @@
 """
 그림과 관련된 함수 및 클래스를 제공하는 모듈입니다.
 line class를 통해 선의 길이를 설정 및 참조를 수행하며
-area_square, area_circle, area_regular_triangle 함수로
-각각 정사각형, 원, 정삼각형의 넓이를 반환합니다.
+외부에서 접근 불가능한 __width와 __height가 있으면,
+변수를 수정하고 접근하기 위해
+set_length와, get_length 메소드를 제공합니다.
 """
 
 import math
 
 class line:
-    """선의 길이에 대해 저장하고 있는 클래스입니다."""
-    __length = 0
+    __width = 0
+    __height = 0
+    def __init__(self, width, height):
+        """초기 width와 height 값을 입력합니다."""
+        self.__width = width
+        self.__height = height
 
-    def __init__(self, length):
-        """선의 길이를 받습니다.(length)"""
-        self.__length = length
-
-    def set_length(self,length):
+    def set_length(self, width, height):
             """선의 길이를 수정합니다."""
-            self.__length = length
+            self.__width = width
+            self.__height = height
 
     def get_length(self):
         """저장된 선의 길이를 반환합니다."""
-        return self.__length
+        return self.__width, self.__height
 
-def area_square(length):
-    """길이를 입력받아 정사각형의 넓이를 구하는 함수입니다."""
-    return length * length
+def area_rectangle(width, height):
+    """길이를 입력받아 직사각형의 넓이를 구하는 함수입니다."""
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height
 
-def area_circle(length):
-    """길이를 입력받아 원의 넓이를 구하는 함수입니다."""
-    return length * length * math.pi
+def area_ellipse(width, height):
+    """길이를 입력받아 타원의 넓이를 구하는 함수입니다."""
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height * math.pi
 
-def area_regular_triangle(length):
-    """길이를 입력받아 정삼각형의 넓이를 구하는 함수입니다."""
-    return length * length * math.sqrt(3) / 4
+def area_right_triangle(width, height):
+    """길이를 입력받아 직삼각형의 넓이를 구하는 함수입니다."""
+    if width <= 0 or height <= 0: raise ValueError
+    return width * height / 2
